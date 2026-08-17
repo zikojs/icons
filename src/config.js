df2d2071546d839fe68482536c3301fb18ddddba
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-08-17 10:22:13 +0100

    remove side effects

---
+++ src/config.js
@@ -0,0 +1,21 @@
+import pc from "picocolors";
+
+export const DEFAULT_TEMPLATES = {
+  spa: { js: "spa-js", ts: "spa-ts", jsx: "spa-jsx", tsx: "spa-tsx" },
+  fbr: { js: "fbr-js", ts: "fbr-ts", jsx: "fbr-jsx" },
+  ssr: { js: "ssr-js", ts: "ssr-ts" },
+};
+
+export const DEFAULT_LANGUAGE_NAMES = {
+  js: pc.yellowBright("JavaScript"),
+  ts: pc.blueBright("TypeScript"),
+  jsx: pc.cyanBright("JSX"),
+  tsx: pc.magentaBright("TSX"),
+};
+
+export const DEFAULT_PROJECT_TYPES = [
+  { name: "Single Page App", value: "spa" },
+  { name: "Single Page App — File-based Router", value: "fbr" },
+  { name: "Server-Side Rendering", value: "ssr" },
+  { name: "Extra", value: "extra" },
+];
